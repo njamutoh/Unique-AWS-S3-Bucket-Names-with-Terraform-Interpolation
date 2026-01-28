7eaf49b3dbc0cc464a522b7263e4e2fba4235c2d
Bertrand Njamutoh <100412514+njamutoh@users.noreply.github.com>
2026-01-27 17:22:46 -0700

    Refactor S3 bucket resources and add tags

--- main.tf
+++ main.tf
@@ -1,36 +1,13 @@
-
 data "aws_region" "current" {}
-
 resource "random_id" "bucket_id" {
   byte_length = 6
 }
-
-resource "random_id" "bucket_id_logs" {
-  byte_length = 6
-}
-
-locals {
-  region        = data.aws_region.current.name
-  bucket_prefix = "${var.project_name}-${var.environment}"
-
-  UniqueID    = random_id.bucket_id.hex
+resource "aws_s3_bucket" "name" {
+  bucket = lower("${var.project_name}-${var.environment}-${data.aws_region.current.name}-${random_id.bucket_id.hex}")
   
-  common_tags = {
+  tags = {
     Project     = var.project_name
     Environment = var.environment
     ManagedBy   = "Terraform"
   }
 }
-
-resource "aws_s3_bucket" "primary_bkt" {
-  bucket = lower("${local.bucket_prefix}-${local.region}-${random_id.bucket_id.hex}")
-
-  tags = local.common_tags
-}
-
-resource "aws_s3_bucket" "logs_bkt" {
-  bucket = lower("${local.bucket_prefix}-${local.UniqueID}")
-
-  tags = local.common_tags
-}
-
